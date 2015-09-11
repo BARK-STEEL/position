4ba3d0ff5a465588c4f7273559224bcc7c685b0a
Steven Creswick <screswick@gmail.com>
2015-09-11 15:22:33 -0400

    initial commit

---
+++ app/models/stock_info.rb
@@ -0,0 +1,13 @@
+
+
+
+def stock_search(symbol)
+  response = HTTParty.get("http://dev.markitondemand.com/Api/v2/Quote?symbol=#{symbol}")
+  @company_name = response['StockQuote']['Name']
+  @last_price = response['StockQuote']['LastPrice']
+  @dollar_change = response['StockQuote']['Change']
+  @change_percent = response['StockQuote']['ChangePercent']
+  @open_price = response['StockQuote']['Open']
+  @high_price = response['StockQuote']['High']
+  @low_price = response['StockQuote']['Low']
+end
