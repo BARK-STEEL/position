a8216a4395b0e022325c6b2011587ce806c4f942
Steven Creswick <screswick@gmail.com>
2015-09-11 15:43:08 -0400

    creates Trade model and controller

---
+++ db/migrate/20150911193155_create_trades.rb
@@ -0,0 +1,13 @@
+class CreateTrades < ActiveRecord::Migration
+  def change
+    create_table :trades do |t|
+      t.integer :user_id
+      t.string :company_symbol
+      t.integer :number_of_shares
+      t.integer :share_purchase_price
+      t.string :trade_type
+
+      t.timestamps null: false
+    end
+  end
+end
